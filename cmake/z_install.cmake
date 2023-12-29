e9f5028513f08f2db4ec05781e50cb832dd88114
shecannotsee <shecannotsee@163.com>
2023-12-29 14:55:06 +0800

    [build] 1.Modification of installation files

--- cmake/z_install.cmake
+++ cmake/z_install.cmake
@@ -19,10 +19,18 @@ install(TARGETS
 if (generate_lib STREQUAL "ON")
     # include install
     install(FILES
-        ${CMAKE_SOURCE_DIR}/src/base64.h # source head
-        DESTINATION # to
-        ${PROJECT_NAME}/include # target dir
+            ${CMAKE_SOURCE_DIR}/src/cpp-stub/stub.h
+            ${CMAKE_SOURCE_DIR}/src/cpp-stub/addr_any.h
+            ${CMAKE_SOURCE_DIR}/src/cpp-stub/addr_pri.h
+            DESTINATION ${PROJECT_NAME}/include/cpp-stub
         )
+    install(FILES
+            ${CMAKE_SOURCE_DIR}/src/class_support.h
+            ${CMAKE_SOURCE_DIR}/src/mock_thread.h
+            ${CMAKE_SOURCE_DIR}/src/std_mutex_support.h
+            ${CMAKE_SOURCE_DIR}/src/std_thread_support.h
+            DESTINATION ${PROJECT_NAME}/include/
+    )
     # lib install
     install(TARGETS
         ${library_static_name} ${library_dynamic_name}
