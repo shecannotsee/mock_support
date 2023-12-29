1492931b1cc8f379ffbf7429cb8ce061e975f64f
shecannotsee <shecannotsee@163.com>
2023-12-29 15:29:28 +0800

    [test] 1.pass test

--- test/t1_thread.cpp
+++ test/t1_thread.cpp
@@ -7,11 +7,12 @@
 #include <iostream>
 #include <thread>
 
+#include "class_support.h"
 #include "global_test_set.h"
 
 TEST(t1_thread, join) {
   // start mock
-  mock_thread m_thread;
+  mock_thread<std::thread, mock::thread> m_thread;
 
   // The following code is derived from: https://en.cppreference.com/w/cpp/thread/thread/join
   auto foo = []() {
@@ -36,4 +37,21 @@ TEST(t1_thread, join) {
   helper2.join();
 
   std::cout << "done!\n";
+  SUCCEED();
+}
+
+TEST(t1_thread, join1) {
+  // start mock
+  mock_thread<std::thread, mock::thread> m_thread;
+
+  // The following code is derived from: https://zh.cppreference.com/w/cpp/thread/thread/detach
+  auto independentThread = []() {
+    std::cout << RED_COLOR << "mock failed\n" << RESET_COLOR;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::cout << RED_COLOR << "mock failed\n" << RESET_COLOR;
+  };
+  std::cout << "Starting thread caller.\n";
+  std::thread t(independentThread);
+  t.detach();
+  std::cout << "Exiting thread caller.\n";
 }
