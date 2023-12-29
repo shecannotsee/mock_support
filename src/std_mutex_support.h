e8cbfeb837542d84c171720e296e889963cdfe2c
shecannotsee <shecannotsee@163.com>
2023-12-29 14:30:53 +0800

    [feat] 1.Add std::mutex mock framework

---
+++ src/std_mutex_support.h
@@ -0,0 +1,13 @@
+//
+// Created by shecannotsee on 23-12-29.
+//
+
+#ifndef STD_MUTEX_SUPPORT_H
+#define STD_MUTEX_SUPPORT_H
+
+namespace mock {
+
+
+}// namespace mock
+
+#endif //STD_MUTEX_SUPPORT_H
