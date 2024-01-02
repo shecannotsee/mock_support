3f44657f9117ac311a5b6e1033c972673a77c867
shecannotsee <shecannotsee@163.com>
2024-01-02 16:50:44 +0800

    [test] 1.mock_timed_mutex add pass

--- test/t2_mutex.cpp
+++ test/t2_mutex.cpp
@@ -15,10 +15,36 @@ TEST(t2_mutex, mutex) {
   mock_mutex mock_mutex_turn_on;
 
   std::mutex test_target;
+  std::cout << BLUE_COLOR << "Member functions start!\n" << RESET_COLOR;
   test_target.lock();
   test_target.unlock();
   test_target.try_lock();
   test_target.native_handle();
+  std::cout << BLUE_COLOR << "Member functions done!\n" << RESET_COLOR;
+  SUCCEED();
+}
+
+TEST(t2_mutex, timed_mutex) {
+  mock_timed_mutex mock_timed_mutex_turn_on;
+
+  std::timed_mutex test_target;
+  std::cout << BLUE_COLOR << "Member functions start!\n" << RESET_COLOR;
+  test_target.lock();
+  test_target.try_lock();
+  // try_lock_for
+  {
+    auto timeout = std::chrono::milliseconds(200);
+    test_target.try_lock_for(timeout);
+  }
+  // try_lock_until
+  {
+    auto now             = std::chrono::system_clock::now();
+    auto tenSecondsLater = now + std::chrono::seconds(10);
+    test_target.try_lock_until(tenSecondsLater);
+  }
+  test_target.unlock();
+  test_target.native_handle();
+  std::cout << BLUE_COLOR << "Member functions done!\n" << RESET_COLOR;
   SUCCEED();
 }
 
@@ -55,7 +81,29 @@ TEST(t2_mutex, unique_lock_with_mutex) {
   SUCCEED();
 }
 
-TEST(t2_mutex, condition_variable) {
+TEST(t2_mutex, unique_lock_with_timed_mutex) {
   SUCCEED();
   return;
+  mock_unique_lock<std::timed_mutex, mock::timed_mutex> mock_unique_lock_turn_on;
+  {
+    std::timed_mutex test_target;
+    std::unique_lock<std::timed_mutex> ul(test_target);
+    std::cout << BLUE_COLOR << "Member functions start!\n" << RESET_COLOR;
+    ul.lock();
+    ul.try_lock();
+    ul.unlock();
+    std::cout << PURPLE_COLOR << "Swap start!\n" << RESET_COLOR;
+    {
+      std::unique_lock<std::timed_mutex> swap_target(test_target);
+      ul.swap(swap_target);
+    }
+    std::cout << PURPLE_COLOR << "Swap done.\n" << RESET_COLOR;
+    ul.release();
+    std::cout << BLUE_COLOR << "Member functions done.\n" << RESET_COLOR;
+  }
+  SUCCEED();
+}
+
+TEST(t2_mutex, condition_variable) {
+  SUCCEED();
 }
